23c9f9110c7eb37b0a72e39a7a0281364f2d8dbd
birdpersonme <birdperson.me@gmail.com>
2025-10-07 21:07:09 +0530

    houdini render with thumbnail

--- script/burnin_hou/render.py
+++ script/burnin_hou/render.py
@@ -7,6 +7,7 @@ from burnin.entity.surreal import Thing
 from burnin.entity.version import Version, VersionStatus
 from burnin.entity.filetype import FileType
 from burnin.entity.utils import TypeWrapper
+from burnin.entity.media import FfmpegCMD
 from burnin_hou.ui import buildFilePath
 
 def burninUSDRenderROP(kwargs):
@@ -37,8 +38,16 @@ def burninUSDRenderROP(kwargs):
 
             trange = node.parm("trange").evalAsString()
             file_name = component_path.split("/")[-1] + "_" + version_number
-            file_name_with_ext = file_name + ".$F4.exr"
+            if trange == "off":
+                file_name_with_ext = file_name + ".exr"
+                thumb_image_file_name_with_ext = file_name_with_ext
+            else:
+                file_name_with_ext = file_name + ".$F4.exr"
+                thumb_image_file_name_with_ext = file_name + "." + str(int(hou.parm("f1").eval())).zfill(4) + ".exr"
+
             full_path = file_path / file_name_with_ext
+            image_file_path = file_path / thumb_image_file_name_with_ext
+            output_thumbnail_path = file_path / "thumbnail.png"
 
             node.parm("outputimage").set(str(full_path))
             node.parm("execute").pressButton()
@@ -49,10 +58,16 @@ def burninUSDRenderROP(kwargs):
             version_type.software = "houdini"
             version_type.status = VersionStatus.Published
 
+            if trange != "off":
+                version_type.head_file = file_name_with_ext.replace("$F4", "####")
+            else:
+                version_type.head_file = file_name_with_ext
+
             ## update node type data : FileTYpe
             file_type: FileType = version_type.file_type.data
             file_type.file_name = file_name
             file_type.file_format = ".exr"
+
             if trange != "off":
                 file_type.time_dependent = True
                 frame_range =  [hou.parm("f1").eval(), hou.parm("f2").eval(), hou.parm("f3").eval()]
@@ -67,7 +82,17 @@ def burninUSDRenderROP(kwargs):
 
             version_node_type: Version = version_node.node_type.data
             node.parm("status").set(version_node_type.status)
+
+            # TODO: This has to be reworked
+            # If the render is only for one frame, the render job completes before the file is being created. 
+            # This causes the thumbnail generator to file.
+
+            if trange != "off":
+                if image_file_path and version_node:
+                    ffmpeg_cmd = FfmpegCMD(image_file_path, output_thumbnail_path)
+                    burnin_client.generate_thumbnail_from_image(ffmpeg_cmd)
+
     
     except Exception as e:
-        hou.ui.displayMessage(f"An error occurred:\n{str(e)}", severity=hou.severityType.Error)
+        hou.ui.displayMessage(f"An error occurred:\n{str(e)}", severity=hou.severityType.Error)#
 
